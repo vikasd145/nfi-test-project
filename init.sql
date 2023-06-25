c5e1c942010d4c9df6f90239bea78d839a9b2ec0
vikas dubey <vikasd145@gmail.com>
2023-06-25 21:57:16 +0800

    db file added

--- init.sql
+++ init.sql
@@ -1,4 +1,6 @@
-CREATE TABLE user_transaction (
-    id SERIAL PRIMARY KEY,
-    balance DOUBLE PRECISION
+CREATE TABLE IF NOT EXISTS user_transaction (
+                id SERIAL PRIMARY KEY,
+                balance DOUBLE PRECISION
 );
+
+CREATE INDEX idx_user_transaction_id ON user_transaction (id);
